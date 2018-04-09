10964d699c74e26436201717ba09e81064074dea
Mike Steder <steder@gmail.com>
2018-04-08 23:35:15 -0500

    Downloading a configurable number of frames based on the time left

--- main.go
+++ main.go
@@ -5,14 +5,25 @@ import (
 	"net/http"
 	"os"
 	"io"
+	"time"
+	"io/ioutil"
+	"encoding/json"
+	"bytes"
+	"github.com/davecgh/go-spew/spew"
+	"math"
 )
 
+/*
+  Documentation: http://192.168.0.135/docs/api/
+
+  http://192.168.0.135/api/v1/printer
+
+ */
 
 const PRINTER_HOST string = "192.168.0.135"
+const DEBUG bool = true
 
-
-func main() {
-	fmt.Printf("Connecting to %s\n", PRINTER_HOST )
+func takeSnapshot(snapshotid int) {
 	snapshot, err := http.Get(fmt.Sprintf("http://%s:8080/?action=snapshot", PRINTER_HOST))
 	if err != nil {
 		// handle it
@@ -20,13 +31,121 @@ func main() {
 	}
 	defer snapshot.Body.Close()
 
-	destPath := fmt.Sprintf("snap.%d.png", 1)
+	destPath := fmt.Sprintf("snap.%d.png", snapshotid)
 	destFile, err := os.Create(destPath)
 	if err != nil {
 		fmt.Printf("Unable to create %s: %s", destPath, err)
 	}
 	defer destFile.Close()
 	os.Chmod(destPath, 0755)
-
+	// TODO check errors here
 	io.Copy(destFile, snapshot.Body)
+	// TODO check more errors here
+}
+
+func takeSnapshots(secondsRemaining float64) {
+	fmt.Printf("Connecting to %s\n", PRINTER_HOST )
+	fps := 30.0
+	var hours float64 = secondsRemaining / 3600.0
+	fmt.Printf("Hours: %v\n", hours)
+	videoDuration := hours * 10.0
+	if videoDuration < 10.0 {
+		videoDuration = 10.0
+	}
+	fmt.Printf("Video Duration: %v\n", videoDuration)
+	snapshotCount := fps * videoDuration
+	delaySeconds := secondsRemaining / snapshotCount
+	if delaySeconds < 2.0 {
+		delaySeconds = 2.0
+		snapshotCount = secondsRemaining / 2.0
+	}
+	snapshotDelay := time.Duration(delaySeconds * 1000.0 * 1000.0 * 1000.0)
+	fmt.Printf("Capturing %f frames for a %fs video at %f FPS\n", snapshotCount, videoDuration, fps)
+	fmt.Printf("Capturing frame every %f seconds for remaining %f seconds\n", snapshotDelay.Seconds(), secondsRemaining)
+	// TODO: Replace with a select statement and a timer?
+ 	for i := 0; i < int(math.RoundToEven(snapshotCount)); i++ {
+		takeSnapshot(i)
+		time.Sleep(snapshotDelay)
+	}
+}
+
+type Printer struct {
+	Status string `json:"status""`
+}
+
+func checkAPI(path string, thing interface{}) {
+	fmt.Printf("Connecting to %s\n", PRINTER_HOST)
+	printerEndpoint := fmt.Sprintf("http://%s/api/v1/%s", PRINTER_HOST, path)
+	printerInfo, err := http.Get(printerEndpoint)
+	if err != nil {
+		// handle it
+		fmt.Println("Unable to load the endpoint: %s", err)
+	}
+	defer printerInfo.Body.Close()
+	jsonBytes, err := ioutil.ReadAll(printerInfo.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body; %s\n", err)
+	}
+	if DEBUG {
+		var debugOutput bytes.Buffer
+		json.Indent(&debugOutput, jsonBytes, "", "\t")
+		fmt.Println(debugOutput.String())
+	}
+	err = json.Unmarshal(jsonBytes, thing)
+	if err != nil{
+		spew.Dump(thing)
+		panic(err)
+	}
+}
+
+func checkPrinterStatus() string {
+	var p Printer;
+	checkAPI("printer", &p)
+	return p.Status
+}
+
+/*
+/api/v1/print_job API response
+
+ */
+
+type PrintJob struct {
+	//Cleaned *time.Time `json:"datetime_cleaned"`
+	//Finished *time.Time `json:"datetime_finished"`
+	Started string `json:"datetime_started"`
+	Name string
+	Progress float64
+	ReprintUUID string `json:"reprint_original_uuid"`
+	Result string
+	Source string
+	Application string `json:"source_application"`
+	User string `json:"source_user"`
+	State string
+	TimeElapsed int `json:"time_elapsed"`
+	TimeTotal int `json:"time_total"`
+	Uuid string
+}
+
+func (j PrintJob) GetStartedTime() (time.Time, error) {
+	return time.Parse("2006-01-02T15:04:05", j.Started)
+}
+
+func getPrintJobTimeRemaining() int {
+	var job PrintJob
+	checkAPI("print_job", &job)
+	start, err := job.GetStartedTime()
+	if err != nil {
+		fmt.Printf("Couldn't parse Started time: %s", err)
+	}
+	fmt.Printf("Job started running at %v with %d seconds remaining", start, job.TimeTotal - job.TimeElapsed)
+	return job.TimeTotal - job.TimeElapsed
+}
+
+func main() {
+	status := checkPrinterStatus()
+	fmt.Printf("Printer Status: %s\n", status)
+	if status == "printing" {
+		timeRemaining := getPrintJobTimeRemaining()
+		takeSnapshots(float64(timeRemaining))
+	}
 }
